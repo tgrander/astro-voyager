532eaf1889166b3ae4e9d5e1389126072a852308
Trey Granderson <treygranderson@gmail.com>
2024-04-28 17:57:34 -0400

    drizzle migrations

---
+++ src/drizzle/migrations/0001_zippy_shape.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "astro_destinations" ALTER COLUMN "average_cost_per_day" SET DATA TYPE integer;--> statement-breakpoint
+ALTER TABLE "astro_destinations" ALTER COLUMN "average_cost_per_day" SET NOT NULL;
