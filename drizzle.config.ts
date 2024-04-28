25b1aa4fd35b6d2c1938ec181f13ca90a2d24050
Trey Granderson <treygranderson@gmail.com>
2024-04-28 15:35:19 -0400

    update drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,9 +1,17 @@
 import type { Config } from "drizzle-kit";
 
+import * as dotenv from "dotenv";
+
 import { env } from "@/env";
+
+dotenv.config({ path: "./.env" });
+
+if (!("DATABASE_URL" in env))
+  throw new Error("DATABASE_URL not found on .env.development");
 
 export default {
   schema: "./src/server/db/schema.ts",
+  out: "./src/db/migrations",
   driver: "pg",
   strict: false,
   dbCredentials: {
