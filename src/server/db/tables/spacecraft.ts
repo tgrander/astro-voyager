9f1241aaee436b306cdf7619fa69e5618f90775e
Trey Granderson <treygranderson@gmail.com>
2024-04-28 12:14:28 -0400

    db: spacecraft table updates

--- src/server/db/tables/spacecraft.ts
+++ src/server/db/tables/spacecraft.ts
@@ -1,8 +1,9 @@
-import { relations, sql } from "drizzle-orm";
+import { sql } from "drizzle-orm";
 import {
   bigint,
   decimal,
   index,
+  integer,
   json,
   pgEnum,
   text,
@@ -13,10 +14,21 @@ import {
 import { createTable } from "@/server/db/create-table";
 
 export const spacecraftTypesEnum = pgEnum("spacecraft_type", [
-  "Shuttle",
-  "Luxury Cruiser",
-  "Economy Shuttle",
-  "Exploration Vessel",
+  "Long-range Cruiser", // For Stellar Explorer and similar vessels
+  "Rugged Explorer", // For Io SandRider and similar rugged terrain vehicles
+  "Mining Shuttle", // For Asteroid Nomad and similar mining operations
+  "Explorer", // General category for exploration oriented spacecraft
+  "Cloud Cruiser", // For Neptune Nimbus and Venus Cloud Cruiser
+  "Ice Explorer", // For Enceladus IceCrafter and similar cold environment vehicles
+  "Space Station", // For Orbit Haven and stationary orbiting platforms
+  "FTL Cruiser", // For Galactic Pioneer and fast-travel interstellar missions
+  "Aerostat", // For aerostatic vehicles like Venusian Serenity
+  "Quantum Yacht", // For Quantum Leap and ultra-luxury, high-tech space yachts
+  "Heat-resistant Shuttle", // For Mercury Beachcomber designed for extreme heat environments
+  "High-altitude Aircraft", // For Venus Cloud Cruiser and similar high-altitude vehicles
+  "Agricultural and Scientific Research Vessel", // For Nebula Harvester
+  "Luxury Rover", // For Mars Cliffhanger, a luxury exploration vehicle on Mars
+  "FTL Starship", // For Interstellar Gateway, capable of faster-than-light travel
 ] as const);
 
 export const spacecraftStatusEnum = pgEnum("spacecraft_status", [
@@ -37,10 +49,7 @@ export const spacecrafts = createTable(
       .notNull()
       .$default(() => []), // array of destination IDs
     capacity: bigint("capacity", { mode: "number" }).notNull(),
-    pricePerDay: decimal("price_per_day", {
-      precision: 10,
-      scale: 2,
-    }).notNull(),
+    pricePerDay: integer("price_per_day").notNull(),
     reviewScore: decimal("review_score", { precision: 3, scale: 2 }),
     imageUrl: text("image_url"),
     heroImage: text("hero_image"),
