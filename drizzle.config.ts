04ca671b06b45629d62705e419466698d92848ef
Trey Granderson <treygranderson@gmail.com>
2024-04-28 13:26:28 -0400

    drizzle.config: `strict: false`

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,11 @@
-import { type Config } from "drizzle-kit";
+import type { Config } from "drizzle-kit";
 
 import { env } from "@/env";
 
 export default {
   schema: "./src/server/db/schema.ts",
   driver: "pg",
+  strict: false,
   dbCredentials: {
     connectionString: env.DATABASE_URL,
   },
