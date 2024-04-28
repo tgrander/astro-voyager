3185403a814c0675ea950c159c6b282fd1d84855
Trey Granderson <treygranderson@gmail.com>
2024-04-28 15:37:48 -0400

    drizzle config: rename out dir

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,17 +1,13 @@
 import type { Config } from "drizzle-kit";
 
-import * as dotenv from "dotenv";
-
 import { env } from "@/env";
-
-dotenv.config({ path: "./.env" });
 
 if (!("DATABASE_URL" in env))
   throw new Error("DATABASE_URL not found on .env.development");
 
 export default {
   schema: "./src/server/db/schema.ts",
-  out: "./src/db/migrations",
+  out: "./src/drizzle/migrations",
   driver: "pg",
   strict: false,
   dbCredentials: {
