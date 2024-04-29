295f6c4183239862eaba3327b8f39b742d1e6f18
Trey Granderson <treygranderson@gmail.com>
2024-04-28 23:16:34 -0400

    sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,18 @@
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://acme.com",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: "https://acme.com/blog",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
+  ];
+}
