d6aedeeb7498459a41e660c988c5adf91d67a1d7
Trey Granderson <treygranderson@gmail.com>
2024-04-28 23:18:25 -0400

    install shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,9 @@
+import { api } from "@/trpc/server";
+
 export default async function Home() {
+  const spacecrafts = await api.spacecraft.search();
+  console.log(spacecrafts);
+
   return (
     <div className="flex h-full w-full items-center justify-center">
       HELLO WORLD
