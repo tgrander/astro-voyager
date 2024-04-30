42751164e0619df2c3cacecc4f9153a8a385095f
Trey Granderson <treygranderson@gmail.com>
2024-04-30 06:36:22 -0400

    background is destination image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ import { cn } from "@/utils";
 
 import { DestinationListingCard } from "./_page";
 
+const backgroundImageUrl =
+  "https://res.cloudinary.com/dfjcoifwd/image/upload/v1714469708/skywalker/destinations/resort-on-mars/ubysijowsnwv4lsbpyiy.png";
+
 export default async function Home() {
   const destinations = await api.destination.search();
 
@@ -11,8 +14,14 @@ export default async function Home() {
       className={cn(
         "relative bg-slate-950 text-white",
         "flex min-h-screen w-full min-w-full flex-col",
+        "bg-cover bg-center bg-no-repeat",
       )}
+      style={{ backgroundImage: `url(${backgroundImageUrl})` }}
     >
+      {/* Overlay with opacity */}
+      <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+
+      {/* Destinations horizontal card list */}
       <section
         className={cn(
           "flex w-full items-center justify-start gap-6 overflow-x-scroll",
