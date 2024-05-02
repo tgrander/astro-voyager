2b096446a9748103764906acfcdc051a11d6fefa
Trey Granderson <treygranderson@gmail.com>
2024-05-02 11:55:55 -0400

    aquarion data

---
+++ src/constants/destinations/aquarion/data.ts
@@ -0,0 +1,58 @@
+import type { NewDestination } from "@/server/db/helpers";
+import { auroraArboretum } from "../aurora-arboretum";
+
+const destinations = [
+  {
+    name: "Sapphire Shores",
+    slug: "sapphire-shores",
+    description:
+      "Sapphire Shores is a breathtaking beach with crystal-clear blue waters and sparkling sand that glimmers under the twin suns, perfect for relaxation and water sports.",
+    heroImage: "skywalker/destinations/sapphire_shores",
+    planet: {
+      name: "Aquarion",
+      slug: "aquarion",
+      image: "",
+    },
+  },
+  {
+    name: "Neptune's Haven",
+    slug: "neptunes-haven",
+    description:
+      "This unique amphibious city, built both above and below the water, features underwater domes and floating platforms, offering a harmonious blend of aquatic and terrestrial lifestyles.",
+    heroImage: "skywalker/destinations/neptunes_haven",
+    planet: {
+      name: "Aquarion",
+      slug: "aquarion",
+      image: "",
+    },
+  },
+  {
+    name: "Mystic Falls",
+    slug: "mystic-falls",
+    description:
+      "Located in a secluded part of Aquarion, Mystic Falls features waterfalls that seem to float before cascading into a luminous pool, surrounded by lush, exotic flora.",
+    heroImage: "skywalker/destinations/mystic_falls",
+    planet: {
+      name: "Aquarion",
+      slug: "aquarion",
+      image: "",
+    },
+  },
+  {
+    name: "Coral Labyrinths",
+    slug: "coral-labyrinths",
+    description:
+      "Explore the Coral Labyrinths, a natural underwater maze teeming with colorful coral formations and vibrant marine life, ideal for diving and adventure.",
+    heroImage: "skywalker/destinations/coral_labyrinths",
+    planet: {
+      name: "Aquarion",
+      slug: "aquarion",
+      image: "",
+    },
+  },
+];
+
+export const data: NewDestination[] = destinations.map((dest) => ({
+  ...auroraArboretum,
+  ...dest,
+}));
--- src/constants/destinations/index.ts
+++ src/constants/destinations/index.ts
@@ -1,15 +1,23 @@
+import type { NewDestination } from "@/server/db/helpers";
+
+// Alien Planet Destinations
+import { data as aquarion } from "./aquarion/data";
 import { auroraArboretum } from "./aurora-arboretum";
+import { data as caelux } from "./caelux/data";
 import { celestialFallsOfEnceladus } from "./celestial-falls-of-enceladus";
 import { luminousLagoonOfVenus } from "./luminous-lagoon-of-venus";
 import { nebulaVineyard } from "./nebula-vineyard";
+import { data as neonix } from "./neonix/data";
 import { novaPolarisStation } from "./nova-polaris-station";
 import { plasmaBeachesOfMercury } from "./plasma-beaches-of-mercury";
 import { quantumQuasarArcade } from "./quantum-quasar-arcade";
 import { redCliffsResortOfMars } from "./red-cliffs-resort-of-mars";
+import { data as seraphis } from "./seraphis/data";
 import { silicaSandsOfIo } from "./silica-sands-of-io";
 import { titansEchoCaverns } from "./titans-echo-caverns";
+import { data as valyrian } from "./valyrian/data";
 
-export const destinationsData = [
+export const destinationsData: NewDestination[] = [
   auroraArboretum,
   novaPolarisStation,
   silicaSandsOfIo,
@@ -20,4 +28,11 @@ export const destinationsData = [
   plasmaBeachesOfMercury,
   luminousLagoonOfVenus,
   quantumQuasarArcade,
+
+  // Alien Planet Destinations
+  ...aquarion,
+  ...caelux,
+  ...neonix,
+  ...seraphis,
+  ...valyrian,
 ] as const;
