7bb5b4fcbe3b359e181a4290ebca36c9114447e6
Trey Granderson <treygranderson@gmail.com>
2024-05-08 20:22:29 -0400

    leva: ambient light

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -2,7 +2,9 @@
 
 import { useControls } from "leva";
 import { Perf } from "r3f-perf";
+import { AmbientLight } from "three";
 
+import type { Schema } from "leva/src/types";
 export default function Page() {
   const ctr = useControls("planet", controls);
 
@@ -11,7 +13,7 @@ export default function Page() {
       {/* Performance */}
       {ctr.perf && <Perf position="top-left" />}
 
-      <ambientLight intensity={0.1} />
+      <ambientLight intensity={ctr.ambientLight} />
       <directionalLight position={[0, 10, 5]} intensity={1} castShadow />
       <color attach="background" args={[ctr.background]} />
 
@@ -24,4 +26,14 @@ export default function Page() {
   );
 }
 
-const controls = { color: "#ffffff", background: "#0C101E", perf: true };
+const controls = {
+  color: "#ffffff",
+  background: "#0C101E",
+  perf: true,
+  ambientLight: {
+    min: 0,
+    max: 5,
+    value: 0.1,
+    step: 0.1,
+  },
+};
