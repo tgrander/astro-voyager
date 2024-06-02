cf417bb27fe1ef8ffd5a006a8213b41d9b3db162
Trey Granderson <treygranderson@gmail.com>
2024-06-02 15:02:58 -0400

    search destinations: X icon to clear selected value

--- src/components/browse-destinations/search/search-bar/destination.tsx
+++ src/components/browse-destinations/search/search-bar/destination.tsx
@@ -2,6 +2,7 @@
 
 import * as React from "react";
 
+import { ChevronsUpDown, X } from "lucide-react";
 import {
   Command,
   CommandEmpty,
@@ -21,7 +22,6 @@ import {
 } from "@/components/ui/popover";
 
 import { Button } from "@/components/ui/button";
-import { ChevronsUpDown } from "lucide-react";
 import { cn } from "@/lib/utils";
 
 export function Destination() {
@@ -37,7 +37,7 @@ export function Destination() {
           role="combobox"
           size="lg"
           aria-expanded={open}
-          className="w-fit justify-between rounded-full"
+          className="flex w-fit items-center justify-center rounded-full"
         >
           <p
             className={cn("text-base text-white", {
@@ -49,9 +49,13 @@ export function Destination() {
                   (destination) =>
                     destination.value === selectedDestination.value,
                 )?.label
-              : "Add destination..."}
+              : "Add destination"}
           </p>
-          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+          <TriggerButtonIcon
+            open={open}
+            isSelected={!!selectedDestination}
+            setSelectedDestination={setSelectedDestination}
+          />
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-[200px] p-0">
@@ -93,5 +97,38 @@ export function Destination() {
         </Command>
       </PopoverContent>
     </Popover>
+  );
+}
+
+function TriggerButtonIcon({
+  open,
+  isSelected,
+  setSelectedDestination,
+}: {
+  open: boolean;
+  isSelected: boolean;
+  setSelectedDestination: React.Dispatch<
+    React.SetStateAction<DestinationListItem | null>
+  >;
+}) {
+  const openAndHasValue = open && isSelected;
+  const className = "h-4 w-4 shrink-0 opacity-50";
+
+  return (
+    <div className="ml-6">
+      {openAndHasValue ? (
+        <div
+          className="rounded-full p-1 hover:bg-gray-600"
+          onClick={(e) => {
+            e.stopPropagation();
+            setSelectedDestination(null);
+          }}
+        >
+          <X className={className} />
+        </div>
+      ) : (
+        <ChevronsUpDown className={className} />
+      )}
+    </div>
   );
 }
