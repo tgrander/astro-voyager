b0f83a4e672708110661a6bd0567501d9575e461
Trey Granderson <treygranderson@gmail.com>
2024-06-02 07:20:31 -0400

    delete old comment

--- src/app/_layout/sidebar-layout/sticky-top.tsx
+++ src/app/_layout/sidebar-layout/sticky-top.tsx
@@ -9,30 +9,3 @@ export function StickyTop() {
     </div>
   );
 }
-
-// {/* Hamburger menu */}
-// <button
-// type="button"
-// className="-m-2.5 p-2.5 text-gray-700 lg:hidden"
-// onClick={() => setSidebarOpen(true)}
-// >
-// <span className="sr-only">Open sidebar</span>
-// <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-// </button>
-// {/* Separator */}
-// <div className="h-6 w-px bg-gray-900/10 lg:hidden" aria-hidden="true" />
-// <div className="flex flex-1 gap-x-4 self-stretch lg:gap-x-6">
-// {/* Search */}
-// <Search />
-// <div className="flex items-center gap-x-4 lg:gap-x-6">
-//   {/* Notifications */}
-//   <Notifications />
-//   {/* Separator */}
-//   <div
-//     className="hidden lg:block lg:h-6 lg:w-px lg:bg-gray-900/10"
-//     aria-hidden="true"
-//   />
-//   {/* Profile dropdown */}
-//   <ProfileDropdown />
-// </div>
-// </div>
