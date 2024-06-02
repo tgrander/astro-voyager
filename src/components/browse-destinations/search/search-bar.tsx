3b7d5f8df00980b937ffbd3a664efb6402644fda
Trey Granderson <treygranderson@gmail.com>
2024-06-02 14:32:11 -0400

    search bar fields are combobox triggers

--- src/components/browse-destinations/search/search-bar.tsx
+++ src/components/browse-destinations/search/search-bar.tsx
@@ -1,69 +1,15 @@
-"use client";
-
-import {
-  HoveredLink,
-  Menu,
-  MenuItem,
-} from "@/components/ui/aceternity/navbar-menu";
-
 import { Destination } from "./search-bar/";
-import type { PlanetsSearch } from "@/api/types";
-import { api } from "@/trpc/react";
 import { cn } from "@/utils/cn";
-import { useState } from "react";
 
 export function SearchBar() {
-  const planetsQuery = api.planet.search.useQuery();
-
-  const planets = planetsQuery.data ?? [];
-
-  return (
-    <div className="relative flex w-full items-center justify-center">
-      <Navbar className="top-2" planets={planets} />
-    </div>
-  );
-}
-
-function Navbar({
-  className,
-  planets,
-}: {
-  className?: string;
-  planets: PlanetsSearch;
-}) {
-  const [active, setActive] = useState<string | null>(null);
-
   return (
     <div
-      className={cn("fixed inset-x-0 top-10 z-50 mx-auto max-w-2xl", className)}
+      className={cn(
+        "relative flex w-full items-center justify-center",
+        "fixed inset-x-0 top-10 z-50 mx-auto max-w-2xl",
+      )}
     >
-      <Menu setActive={setActive}>
-        <MenuItem
-          setActive={setActive}
-          active={"Destination"}
-          item="Destination"
-        >
-          <Destination />
-        </MenuItem>
-
-        <MenuItem setActive={setActive} active={active} item="Dates">
-          <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/web-dev">Web Development</HoveredLink>
-            <HoveredLink href="/interface-design">Interface Design</HoveredLink>
-            <HoveredLink href="/seo">Search Engine Optimization</HoveredLink>
-            <HoveredLink href="/branding">Branding</HoveredLink>
-          </div>
-        </MenuItem>
-
-        <MenuItem setActive={setActive} active={active} item="Who">
-          <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/hobby">Hobby</HoveredLink>
-            <HoveredLink href="/individual">Individual</HoveredLink>
-            <HoveredLink href="/team">Team</HoveredLink>
-            <HoveredLink href="/enterprise">Enterprise</HoveredLink>
-          </div>
-        </MenuItem>
-      </Menu>
+      <Destination />
     </div>
   );
 }
--- src/components/browse-destinations/search/search-bar/destination.tsx
+++ src/components/browse-destinations/search/search-bar/destination.tsx
@@ -14,51 +14,81 @@ import {
   Destination as DestinationListItem,
   destinations,
 } from "@/data/search";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 
+import { Button } from "@/components/ui/button";
+import { ChevronsUpDown } from "lucide-react";
 import { cn } from "@/lib/utils";
 
 export function Destination() {
   const [open, setOpen] = React.useState(false);
-  const [selectedStatus, setSelectedStatus] =
+  const [selectedDestination, setSelectedDestination] =
     React.useState<DestinationListItem | null>(null);
 
   return (
     <div className="flex items-center space-x-4">
-      <Command>
-        <CommandInput
-          placeholder="Search destination..."
-          className="text-base"
-        />
-        <CommandList>
-          <CommandEmpty>No results found.</CommandEmpty>
-          <CommandGroup>
-            {destinations.map((destination) => (
-              <CommandItem
-                key={destination.value}
-                value={destination.value}
-                className="text-base"
-                onSelect={(value) => {
-                  setSelectedStatus(
-                    destinations.find((priority) => priority.value === value) ||
-                      null,
-                  );
-                  setOpen(false);
-                }}
-              >
-                <destination.icon
-                  className={cn(
-                    "mr-5 h-4 w-4",
-                    destination.value === selectedStatus?.value
-                      ? "opacity-100"
-                      : "opacity-40",
-                  )}
-                />
-                <span>{destination.label}</span>
-              </CommandItem>
-            ))}
-          </CommandGroup>
-        </CommandList>
-      </Command>
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            role="combobox"
+            aria-expanded={open}
+            className="w-[200px] justify-between"
+          >
+            {selectedDestination ? (
+              destinations.find(
+                (destination) =>
+                  destination.value === selectedDestination.value,
+              )?.label
+            ) : (
+              <p className="text-base text-white/80">Add destination</p>
+            )}
+            <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-[200px] p-0">
+          <Command>
+            <CommandInput
+              placeholder="Search destination..."
+              className="text-base"
+            />
+            <CommandList>
+              <CommandEmpty>No results found.</CommandEmpty>
+              <CommandGroup>
+                {destinations.map((destination) => (
+                  <CommandItem
+                    key={destination.value}
+                    value={destination.value}
+                    className="text-base"
+                    onSelect={(value) => {
+                      setSelectedDestination(
+                        destinations.find(
+                          (priority) => priority.value === value,
+                        ) || null,
+                      );
+                      setOpen(false);
+                    }}
+                  >
+                    <destination.icon
+                      className={cn(
+                        "mr-5 h-4 w-4",
+                        destination.value === selectedDestination?.value
+                          ? "opacity-100"
+                          : "opacity-40",
+                      )}
+                    />
+                    <span>{destination.label}</span>
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </CommandList>
+          </Command>
+        </PopoverContent>
+      </Popover>
     </div>
   );
 }
